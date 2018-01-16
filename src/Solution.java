3b2ebff1efcf52a57df47403cfa5844994c77cd3
Rui <rui@dhcp108.merusouth114.iit.edu>
2018-01-15 23:19:18 -0600

    first commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public class Solution {
-
+	public List<Integer> grayCode(int n) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        	
+        for(int i = 0; i < Math.pow(2, n); i++)
+        {
+        	res.add((i >> 1) ^ i);
+        }
+        
+        return res;
+    }
 }
